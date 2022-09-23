05f673dafc236bee98dcec21cbec4a8888a80b58
soyezequiel <alcayaga147147@gmail.com>
2022-09-23 21:04:11 +0000

    se agrego migraciones nesesarias para que el monstruo tenga fecha de nacimiento y la opcion de privacidad para pener en privado su twitter

--- db/migrate/20220923140100_add_details_to_monster_privacidad.rb
+++ db/migrate/20220923140100_add_details_to_monster_privacidad.rb
@@ -1,6 +1,7 @@
 class AddDetailsToMonsterPrivacidad < ActiveRecord::Migration[7.0]
   def change
-    add_column :monsters, :estado_privado, :integer
-    
+    add_column :monsters, :privacidad_activada, :integer
+    add_column :monsters, :birthdate, :date
   end
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_23_133359) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_23_140100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_23_133359) do
     t.integer "scare_level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "privacidad_activada"
+    t.date "birthdate"
   end
 
   create_table "tweeters", force: :cascade do |t|
